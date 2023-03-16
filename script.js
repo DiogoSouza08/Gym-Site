be13110bb6779cf05c626ae660c8d9ab99c30cef
Diogo Souza <115508662+DiogoSouza08@users.noreply.github.com>
2023-03-16 10:46:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// scrollbar color
+const scrollableContainer = document.querySelector('.scrollable-container');
+
+scrollableContainer.style.setProperty('--scrollbar-color', '#f5f5f5');
+scrollableContainer.style.setProperty('--thumb-color', '#333333');
+
